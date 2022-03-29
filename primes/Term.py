45e09a3c98d3818ad206fba0d23783540ba8ecfc
Luis Galindo <egalindo54@uabc.edu.mx>
2022-03-29 09:48:11 -0700

    bindigits anadido a Term

--- primes/Term.py
+++ primes/Term.py
@@ -1,11 +1,22 @@
 
 class Term():
 
-    def __init__(self, implicats, binary):
-        self.implicatns = implicants
+    def __init__(self, implicatns, binary):
+        self.implicatns = implicatns
         self.binary = binary
         self.n_ones = Term.count_ones(binary)
 
+
+    @classmethod
+    def from_int(cls: type, number: int, n_variables: int) -> type:
+        return cls([value], Term.bindigits(number, n_variables))
+
+    @classmethod
+    def bindigits(cls: type, number: int,digits: int) -> str:
+        """ Convertir numero a binario de tamaño fijo """
+        return "{0:0{1}b}".format(number,digits)
+
+        
     @classmethod
     def count_ones(cls, a: str) -> int:
         """ Cuenta cuantos '1' aparecen en un string """
