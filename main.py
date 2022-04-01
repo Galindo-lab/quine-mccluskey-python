efc741fb1e6247dbf72e288ac281cf05c242498a
Luis Galindo <egalindo54@uabc.edu.mx>
2022-04-01 01:32:46 -0700

    input eliminado

--- main.py
+++ main.py
@@ -2,25 +2,15 @@
 from primes.find_primes import prime_implicants
 from primes.prime_chart import solve_chart
 
+from primes.Input import Input
+
 def print_table(m: list) -> None:
     """ Implime los valores de una tabla  """
     for i in m:
         print(i.binary, '|', i.implicants)
-
-def capture_function(n_variables):
-    """  """
-    funcion, redundancia = [], []
-    for i in range(0,1<<n_variables):
-        captura = input(str(i)+ ": ")
-        if captura == '1':
-            funcion.append(i)
-        elif captura == '-':
-            redundancia.append(i)
-    return funcion, redundancia
             
 def x():
-    n = int(input("Numero de variables: "))
-    m, d = capture_function(n)
+    n,m,d = Input.from_table()
 
     # n = 4
     # m = [4,8,10,11,12,15]
