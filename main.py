7a2fc36d11be92fcf6015146b1c52cdbd720bea4
Luis Galindo <egalindo54@uabc.edu.mx>
2022-04-05 20:50:24 -0700

    main reformateado

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
-
 from src.find_primes import prime_implicants
 from src.prime_chart import solve_chart
 
 from src.Input import Input
 
+
+def clear_screen() -> None:
+    """ Dejar la pantalla lo mas limpia posible """
+    print("\n" * 7)
+
+
+def print_title() -> None:
+    """ Imprime el titulo del programa """
+    print("\n".join([
+        "---------------------",
+        "      REDUCCION      ",
+        "  DE FUNCION LOGICA  ",
+        "---------------------\n",
+    ]))
+
+
+def select_input_type() -> tuple:
+    """ Seleccionar como ingresar los datos al programa """
+    while (1):
+        print("0.LISTA / 1.TABLA?")
+        e = input()
+        if e == '0':
+            return Input.from_list()
+        elif e == '1':
+            return Input.from_table()
+        else:
+            print("TIPO INVALIDO")
+
+
 def print_table(m: list) -> None:
-    """ Implime los valores de una tabla  """
+    """ Implime los valores de una tabla """
     for i in m:
         print(i.binary, '|', i.implicants)
-            
+
+
 def x():
-    # n,m,d = Input.from_table()
-    n,m,d = Input.from_list()
+    clear_screen()
+    print_title()
 
-    # n = 4
-    # m = [4,8,10,11,12,15]
-    # d = [9,14]
+    n, m, d = select_input_type()
 
-    # n = 4
-    # m = [0,1,11,13,15]
-    # d = []
-    
-    # print("Tabla" + (10 * '-'))
-    solve_chart(n,m,d)
-    # print_table(prime_implicants(n, m, d))
-
-x()
+    solve_chart(n, m, d)
 
 
+#x()
